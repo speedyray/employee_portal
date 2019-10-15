afaac80e25cbe54160981decfff7cd76cf863b55
speedyray <speedyray2ray@gmail.com>
2019-10-14 22:37:44 -0400

    Added routing using react-router

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import './App.css';
 import Home from './components/Home';
 import Department from './components/Department';
 import Employee from './components/Employee';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="container">
-     <Home/>
-     <Department/>
-     <Employee/>
+     <Switch>
+       <Route path='/' component={Home} exact />
+       <Route path='/department' component={Department} />exact />
+       <Route path='/employee' component={Employee} />exact />
+     </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
