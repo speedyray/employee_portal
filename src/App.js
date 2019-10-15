ebc4ced2a80a4fa572d1618b0c63561e874e76ea
speedyray <speedyray2ray@gmail.com>
2019-10-15 00:14:56 -0400

    Added Navigation and links to portal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
 import Department from './components/Department';
 import Employee from './components/Employee';
+import Navigation from './components/Navigation';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-
 
 function App() {
   return (
     <BrowserRouter>
     <div className="container">
+       <h3 className="m-3 d-flex justify-content-center">
+         React JS with web api Demo</h3>
+         <h5 className="m-3 d-flex justify-content-center">
+         Employee Management Portal</h5>
+         
+       <Navigation />
+
      <Switch>
        <Route path='/' component={Home} exact />
        <Route path='/department' component={Department} />exact />
--- src/components/Department.js
+++ src/components/Department.js
@@ -1,14 +1,45 @@
 import React, {Component} from 'react';
+import {Table} from 'react-bootstrap';
 
 
 class Department extends Component{
+    constructor(props){
+     super(props);
+      this.state = {deps: []}
+
+ }
+
+ componentDidMount(){
+      this.refreshList();
+ }
+
+ refreshList(){
+     this.setState({
+         deps:[{"DepartmentID": 1, "DepartmentName": "Information Technology"},
+               {"DepartmentID": 2, "DepartmentName": "Human Capital"}]
+     })
+ }
     render(){
+     const {deps} = this.state
         return(
-        <div className="mt-5 d-flex justify-content-left">
-            <h3>
-                This is the Department page
-            </h3>
-        </div>
+           <Table className="mt-4" striped bordered hover size="sm">
+               <thead>
+                    <tr>
+                         <th>DepartmentID</th>
+                         <th>DepartmentName </th>
+                    </tr>
+               </thead>
+               <tbody>
+                   {deps.map(dep=> 
+                        <tr  key={dep.DepartmentID}>
+                        <td>{dep.DepartmentID}</td>
+                        <td>{dep.DepartmentName}</td>
+                        
+                        </tr>
+                        
+                        )}
+               </tbody>
+           </Table>
         )
     }
 }
