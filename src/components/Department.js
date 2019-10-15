7f9e3ce9f2c730588d6f9f1a15040d4fba878d5c
speedyray <speedyray2ray@gmail.com>
2019-10-14 22:21:35 -0400

    created 3 components

---
+++ src/components/Department.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class Department extends Component{
+    render(){
+        return(
+        <div className="mt-5 d-flex justify-content-left">
+            <h3>
+                This is the Department page
+            </h3>
+        </div>
+        )
+    }
+}
+
+
+
+export default Department
---
+++ src/components/Employee.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class Employee extends Component{
+    render(){
+        return(
+        <div className="mt-5 d-flex justify-content-left">
+            <h3>
+                This is the Employee page
+            </h3>
+        </div>
+        )
+    }
+}
+
+
+
+export default Employee;
